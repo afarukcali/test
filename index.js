6535a6951ad8bf7489607641a83de7cd241974a6
afarukcali <afarukcali@gmail.com>
2022-08-17 15:25:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const axios = require("axios");
+const sha256context = require("sha256context");
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/result", (req, res) => {
+  const { text } = req.query;
+  const sha256contextResult = sha256context(text);
+
+  res.render("result.ejs", { result: sha256contextResult });
+});
+
+app.listen(3005);
